6c34223a0828c3a743d750f41911d8390bbb1672
ananyagoyal1001 <145464347+ananyagoyal1001@users.noreply.github.com>
2025-03-21 11:35:44 +0100

    Update App.py

--- App.py
+++ App.py
@@ -16,7 +16,7 @@ with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 
-authenticator = Authenticate(
+authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
