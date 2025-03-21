2548118fb5ee247e1b072b70f8f5d9049d4a2561
ananyagoyal1001 <145464347+ananyagoyal1001@users.noreply.github.com>
2025-03-21 11:34:56 +0100

    Update App.py

--- App.py
+++ App.py
@@ -15,6 +15,15 @@ from yaml.loader import SafeLoader
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
 name, authentication_status, username = authenticator.login('Login', 'main')
 
 if authentication_status:
