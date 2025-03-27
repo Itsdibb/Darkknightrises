eefa79dc339e6980323b04d6d8ca777023f33ee1
ananyagoyal1001 <145464347+ananyagoyal1001@users.noreply.github.com>
2025-03-27 11:24:17 +0100

    Update App.py

--- App.py
+++ App.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.title("Authenticate")
 
 if not st.experimental_user.is_logged_in:
-    if st.button("Authenticate"):
-        st.login("auth0")
+    if st.button("Log in"):
+        st.login()
 
 st.json(st.experimental_user)
 
